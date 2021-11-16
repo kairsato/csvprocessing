e53d5aea5ef5081d614280e48040846e3af94df0
Kai <kairobbiesato@gmail.com>
2021-11-16 18:49:00 +1000

    added stdout with specified format

--- main.py
+++ main.py
@@ -9,23 +9,31 @@ import os
 import sys
 
 #defining filename
-print("Enter input filename(include file extension): ")
-filename = input()
+print("Enter input filename(include file extension): ", end='')
+inputFilename = input()
+
 
 #checking if it exists and exiting if it doesn't
-if not os.path.exists(filename):
-    print("Cannot find input file "+filename, file=sys.stderr)
+if not os.path.exists(inputFilename):
+    print("Cannot find input file "+inputFilename, file=sys.stderr)
     sys.exit(1)
 
 #reading data
-df = pd.read_csv(filename)
+df = pd.read_csv(inputFilename)
 
 #sorting both fields
-df = df.sort_values(["division", "points"], ascending=(True, True))
+df = df.sort_values(["division", "points"], ascending=(True, False))
+
+#printing out the results as specified
+print("records:")
+for i in range(0,3):
+    print("- name: "+ str(df["firstname"].iloc[i]) + " " + str(df["lastname"].iloc[i]))
+    print("  details: In division "+ str(df["division"].iloc[i]) + " from " + str(df["date"].iloc[i]) + " performing " + str(df["summary"].iloc[i]))
 
 
 
 
-print(df)
+
+
 
 
