2081d7000c8b2e16cbc804e4c895d124ced852e6
Kai <kairobbiesato@gmail.com>
2021-11-16 17:43:25 +1000

    imported pandas, read input and sort fields

--- main.py
+++ main.py
@@ -4,3 +4,15 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
+import pandas as pd
+
+#reading in input
+filename = "input.csv"
+df = pd.read_csv(filename)
+
+#sorting both fields
+df = df.sort_values(["division", "points"], ascending=(True, True))
+
+print(df)
+
+
